b40add2830bc6e83046bfb1469b7ea45feb5ac9c
zaval <zavalieblodaun@gmail.com>
2023-03-02 20:27:52 +0200

    resolved conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -47,11 +47,6 @@ class DogsListApp extends StatelessWidget {
           },
         )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
