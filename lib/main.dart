904d37bc8584dcae4d9a3a289c1030bd975228b0
zaval <zavalieblodaun@gmail.com>
2023-03-17 00:31:22 +0200

    Create second screen and add navigation in breed widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dogs_list/presentation/breed_screen.dart';
+import 'package:dogs_list/presentation/dogs_breed_screen/breed_screen.dart';
 import 'package:dogs_list/setup_service_locator.dart';
 import 'package:flutter/material.dart';
 
@@ -8,3 +8,5 @@ void main() {
   setupServiceLocator();
   runApp(const BreedScreen());
 }
+//1) Добавить в правильном месте по нажатию на породу навигацию
+//пойти внутрь виджета с породами
