496ce6a204fe3673e5e09c838e1c0fa8c494f793
zaval <zavalieblodaun@gmail.com>
2023-03-17 23:23:30 +0200

    show image on screen

--- lib/presentation/dogs_image_screen/dogs_image_screen.dart
+++ lib/presentation/dogs_image_screen/dogs_image_screen.dart
@@ -1,11 +1,18 @@
+import 'package:dogs_list/business_logic/bloc/image_list_bloc.dart';
+import 'package:dogs_list/data/model/breed_model.dart';
+import 'package:dogs_list/data/repository/repository.dart';
 import 'package:dogs_list/presentation/dogs_image_screen/dogs_image_layout.dart';
+import 'package:dogs_list/setup_service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DogsImagesScreen extends StatelessWidget {
-  const DogsImagesScreen({Key? key}) : super(key: key);
-
+  const DogsImagesScreen({Key? key, required this.breed}) : super(key: key);
+  final Breed breed;
   @override
   Widget build(BuildContext context) {
-    return const DogsImagesLayout();
+    return BlocProvider(
+        create: (_) => ImageListBloc(getIt<Repository>()),
+        child: DogsImagesLayout(breed: breed,));
   }
 }
