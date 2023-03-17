315432b2feb98122771a8fed16cdfca14f34d34e
zaval <zavalieblodaun@gmail.com>
2023-03-17 23:26:21 +0200

    added a new function on network services

--- lib/data/services/network_services.dart
+++ lib/data/services/network_services.dart
@@ -2,4 +2,5 @@ import 'package:dogs_list/data/model/breed_model.dart';
 
 abstract class NetworkService{
   Future<Map<String, dynamic>> fetchBreeds();
+  Future<List<String>> fetchImages(Breed breed);
 }
