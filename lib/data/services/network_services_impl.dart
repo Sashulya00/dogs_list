df7c8aed2728c11466f8ced1c638b5af3e85c403
zaval <zavalieblodaun@gmail.com>
2023-03-30 00:04:51 +0300

    delete space and divide images api

--- lib/data/services/network_services_impl.dart
+++ lib/data/services/network_services_impl.dart
@@ -4,14 +4,14 @@ import 'package:dogs_list/data/model/breed_model.dart';
 import 'package:dogs_list/data/services/network_services.dart';
 import 'package:http/http.dart';
 
-
 class NetworkServiceImpl implements NetworkService {
   static const _baseUrl = "https://dog.ceo";
   static const _apiUrl = "$_baseUrl/api";
   static const _dogListEndpointUrl = '$_apiUrl/breeds/list/all';
   static const _messageKey = 'message';
+  static const _imagesEndPoint = '$_baseUrl/api/breed';
+  static const _randomImagesKey = 'images/random';
   static const _countImages = 10;
-
 
   @override
   Future<Map<String, dynamic>> fetchBreeds() async {
@@ -22,15 +22,12 @@ class NetworkServiceImpl implements NetworkService {
     return messageMap;
   }
 
-
   @override
   Future<List<String>> fetchImages(Breed breed) async {
-    final imgUrl = Uri.parse('https://dog.ceo/api/breed/${breed.breed}/images/random/$_countImages');
+    final imgUrl = Uri.parse('$_imagesEndPoint/${breed.breed}/$_randomImagesKey/$_countImages');
     var response = await get(imgUrl);
     Map<String, dynamic> responseMap = json.decode(response.body);
     final imageList = List<String>.from(responseMap[_messageKey]);
     return imageList;
   }
-
-
 }
