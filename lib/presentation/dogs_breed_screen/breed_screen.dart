7f64efe6c0a73fcbbdd34509d0cdceaca5b8926b
zaval <zavalieblodaun@gmail.com>
2023-03-29 23:58:29 +0300

    refactor bloc method as provider and delete unused library

--- lib/presentation/dogs_breed_screen/breed_screen.dart
+++ lib/presentation/dogs_breed_screen/breed_screen.dart
@@ -1,17 +1,18 @@
-import 'package:dogs_list/business_logic/bloc/breed_list_bloc.dart';
+import 'package:dogs_list/business_logic/provider/fetchbreed_provider.dart';
 import 'package:dogs_list/data/repository/repository.dart';
 import 'package:dogs_list/presentation/dogs_breed_screen/breed_layout.dart';
 import 'package:dogs_list/setup_service_locator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:provider/provider.dart';
 
 class BreedScreen extends StatelessWidget {
-  const BreedScreen({Key? key}) : super(key: key);
+  const BreedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create: (_) => DogListBloc(getIt<Repository>()),
-      child: const BreedLayout(),);
+    return ChangeNotifierProvider(
+      create: (_) => BreedProvider(getIt<Repository>()),
+      child: const BreedLayout(),
+    );
   }
 }
