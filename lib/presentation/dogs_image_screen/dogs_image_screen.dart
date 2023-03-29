3af7638590da2d7fad532389285e6f7e0deea8fa
zaval <zavalieblodaun@gmail.com>
2023-03-30 00:08:05 +0300

    delete bloc lib, add a provider lib and refactor code with provider

--- lib/presentation/dogs_image_screen/dogs_image_screen.dart
+++ lib/presentation/dogs_image_screen/dogs_image_screen.dart
@@ -1,18 +1,21 @@
-import 'package:dogs_list/business_logic/bloc/image_list_bloc.dart';
+import 'package:dogs_list/business_logic/provider/fetchimages_provider.dart';
 import 'package:dogs_list/data/model/breed_model.dart';
 import 'package:dogs_list/data/repository/repository.dart';
 import 'package:dogs_list/presentation/dogs_image_screen/dogs_image_layout.dart';
 import 'package:dogs_list/setup_service_locator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 class DogsImagesScreen extends StatelessWidget {
-  const DogsImagesScreen({Key? key, required this.breed}) : super(key: key);
+  const DogsImagesScreen({super.key, required this.breed});
+
   final Breed breed;
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (_) => ImageListBloc(getIt<Repository>()),
-        child: DogsImagesLayout(breed: breed,));
+    return ChangeNotifierProvider(
+      create: (_) => ImagesProvider(getIt<Repository>()),
+      child: DogsImagesLayout(breed: breed),
+    );
   }
 }
